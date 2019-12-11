18f5edffe22eb004d47a95a8f9249d85e581cfea
nagamocha3000 <nagamocha@gmail.com>
2019-12-11 14:22:55 +0300

    complete day 1

---
+++ src/solutions/Day1/task1_2.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+
+	utils "github.com/nagamocha3000/aoc2019/src/helpers"
+)
+
+func getModuleWeights(r io.Reader) ([]int, error) {
+	scanner := bufio.NewScanner(r)
+	var moduleWeights []int
+	for scanner.Scan() {
+		num, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return nil, err
+		}
+		moduleWeights = append(moduleWeights, num)
+	}
+	return moduleWeights, nil
+}
+
+func getTotalFuel(moduleWeights []int, getFuel func(int) int) int {
+	totalFuel := 0
+	for _, weight := range moduleWeights {
+		totalFuel += getFuel(weight)
+	}
+	return totalFuel
+}
+
+func getFuelTask1(weight int) int {
+	return (weight / 3) - 2
+}
+
+func getFuelTask2(weight int) int {
+	fuelRequired := (weight / 3) - 2
+	if fuelRequired < 0 {
+		return 0
+	}
+	return fuelRequired + getFuelTask2(fuelRequired)
+}
+
+func main() {
+	//read file contents
+	file, err := os.Open("./input.txt")
+	utils.CheckErr(err)
+	defer file.Close()
+
+	//get module weights
+	moduleWeights, err := getModuleWeights(file)
+	utils.CheckErr(err)
+
+	//Task 1, get fuel: 3150224
+	totalFuel := getTotalFuel(moduleWeights, getFuelTask1)
+	fmt.Printf("Task1: Total fuel required= %d\n", totalFuel)
+
+	//Task 2, get fuel + fuel's fuel: 4722484
+	totalFuel = getTotalFuel(moduleWeights, getFuelTask2)
+	fmt.Printf("Task2: Total fuel required= %d\n", totalFuel)
+}
---
+++ src/solutions/Day1/task1_2_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func TestTask1(t *testing.T) {
+	casesTask2 := []struct {
+		Weight       int
+		ExpectedFuel int
+	}{
+		{14, 2},
+		{1969, 966},
+		{100756, 50346},
+	}
+
+	assertCorrectFuel := func(t *testing.T, got, want int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	}
+
+	t.Run("test get fuel for task 2", func(t *testing.T) {
+		for _, test := range casesTask2 {
+			got := getFuelTask2(test.Weight)
+			assertCorrectFuel(t, got, test.ExpectedFuel)
+		}
+	})
+
+}
