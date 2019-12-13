d3a8367db882ffb5aa3543e9846ab02f5ccf52ab
nagamocha3000 <nagamocha@gmail.com>
2019-12-13 08:58:21 +0300

    setup for day3

--- src/solutions/day03/task03.go
+++ src/solutions/day03/task03.go
@@ -1,10 +1,137 @@
 package day03
 
 import (
+	"errors"
 	"fmt"
 	"path/filepath"
 	"runtime"
+	"strconv"
+	"strings"
 )
+
+//Dir
+type towards int
+
+const (
+	up towards = iota
+	down
+	left
+	right
+)
+
+type direction struct {
+	towards
+	steps int
+}
+
+var errInvalidDirStr = errors.New("Invalid direction string")
+
+func parseDirection(singleDirStr string) (direction, error) {
+	dir := direction{}
+	num, err := strconv.Atoi(singleDirStr[1:])
+	dir.steps = num
+	switch singleDirStr[0] {
+	case 'R':
+		dir.towards = right
+	case 'L':
+		dir.towards = left
+	case 'U':
+		dir.towards = up
+	case 'D':
+		dir.towards = down
+	default:
+		err = errInvalidDirStr
+	}
+	return dir, err
+}
+
+func parseMultipleDirections(dirStr string) ([]direction, error) {
+	var dirs []direction
+	dirStrSlice := strings.Split(dirStr, ",")
+	for _, dirStr := range dirStrSlice {
+		dir, err := parseDirection(dirStr)
+		if err != nil {
+			return dirs, err
+		}
+		dirs = append(dirs, dir)
+	}
+	return dirs, nil
+}
+
+type circuitBoard struct {
+	width  int
+	height int
+	board  [][]int
+}
+
+func newCircuitBoard(width, height int) circuitBoard {
+	board := make([][]int, height)
+	for i := 0; i < height; i++ {
+		board[i] = make([]int, width)
+	}
+	cb := circuitBoard{width: width, height: height, board: board}
+	return cb
+}
+
+type point struct {
+	x int
+	y int
+}
+
+var errInvalidPoint = errors.New("Invalid point")
+
+func (cb *circuitBoard) markPoint(p point) error {
+	if p.x < 0 || p.x > cb.width || p.y < 0 || p.y >= cb.height {
+		return errInvalidPoint
+	}
+	cb.board[p.y][p.x] = 1
+	return nil
+}
+
+func (cb *circuitBoard) markLine(startingPt point, dir direction) {
+	currPt := startingPt
+	for i := 0; i < dir.steps; i++ {
+		nextPt := getAdjacentPoint(currPt, dir)
+		err := cb.markPoint(nextPt)
+		if err != nil {
+			panic(err)
+		}
+		currPt = nextPt
+	}
+}
+
+func getManhattanDistance(p1, p2 point) int {
+	abs := func(i int) int {
+		if i < 0 {
+			return -i
+		}
+		return i
+	}
+	return abs(p1.x-p2.x) + abs(p1.y-p2.y)
+}
+
+func getNextPoint(currPt point, t towards, steps int) point {
+	nextPt := currPt
+	switch t {
+	case up:
+		nextPt.y -= steps
+	case down:
+		nextPt.y += steps
+	case left:
+		nextPt.x -= steps
+	case right:
+		nextPt.x += steps
+	}
+	return nextPt
+}
+
+func getEndPoint(startingPt point, dir direction) point {
+	return getNextPoint(startingPt, dir.towards, dir.steps)
+}
+
+func getAdjacentPoint(startingPt point, dir direction) point {
+	return getNextPoint(startingPt, dir.towards, 1)
+}
 
 //Answer ..
 func Answer() {
--- src/solutions/day03/task03_test.go
+++ src/solutions/day03/task03_test.go
@@ -1,1 +1,65 @@
 package day03
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseSingleDirection(t *testing.T) {
+
+	assertCorrectDirection := func(t *testing.T, got, want direction) {
+		t.Helper()
+		if got.towards != want.towards || got.steps != want.steps {
+			t.Errorf("Got %v, want %v\n", got, want)
+		}
+	}
+
+	assertError := func(t *testing.T, err error) {
+		t.Helper()
+		if err == nil {
+			t.Errorf("Got %v. Want non-nil error\n", err)
+		}
+	}
+
+	t.Run("test parse single direction", func(t *testing.T) {
+		want := direction{right, 9090}
+		got, _ := parseDirection("R9090")
+		assertCorrectDirection(t, got, want)
+	})
+
+	t.Run("test parse single direction with invalid dir", func(t *testing.T) {
+		_, err := parseDirection("X9090")
+		assertError(t, err)
+	})
+
+	t.Run("test parse single direction with invalid num string", func(t *testing.T) {
+		_, err := parseDirection("R90,")
+		assertError(t, err)
+	})
+
+}
+
+func TestParseMultipleDirections(t *testing.T) {
+	assertNoError := func(t *testing.T, err error) {
+		t.Helper()
+		if err != nil {
+			t.Errorf("Got %v. Want nil\n", err)
+		}
+	}
+
+	assertCorrectDirections := func(t *testing.T, got, want []direction) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("\ngot %v \nwant %v\n", got, want)
+		}
+	}
+	dirStr := "R8,U5,L5,D3"
+	want := []direction{
+		direction{right, 8},
+		direction{up, 5},
+		direction{left, 5},
+		direction{down, 3}}
+	got, err := parseMultipleDirections(dirStr)
+	assertNoError(t, err)
+	assertCorrectDirections(t, got, want)
+}
